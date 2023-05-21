7423ecfa3e555a61d1deb18c1f39a618bf16f65d
shitchell <shauni3dud3@gmail.com>
2023-05-21 01:01:34 +0000

    adding Johnny's work (aflynch)

--- pente.babylon.js
+++ pente.babylon.js
@@ -1,30 +1,117 @@
 function createScene() {
+  // This creates a basic Babylon Scene object (non-mesh)
   var scene = new BABYLON.Scene(engine);
+  var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 8, 50, BABYLON.Vector3.Zero(), scene);
 
-  
+  //  BABYLON.FlyCamera
   // This creates and positions a free camera (non-mesh)
-  var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-  
+  // var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(50, 50, -100), scene);
+
   // This targets the camera to scene origin
-  camera.setTarget(BABYLON.Vector3.Zero());
-  
+  //  camera.setTarget(BABYLON.Vector3.Zero());
+
   // This attaches the camera to the canvas
   camera.attachControl(canvas, true);
-  
+
+  // var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 8, 50, BABYLON.Vector3.Zero(), scene);
+
+  // camera.attachControl(canvas, true);
+
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
   var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
-  
+
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
-  
+
   // Our built-in 'sphere' shape. Params: name, options, scene
-  var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {diameter: 2, segments: 32}, scene);
-  
-  // Move the sphere upward 1/2 its height
-  sphere.position.y = 1;
-  
+  var sphereArray = [];
+  // Move the sphere upward 1/2 its 
+  const spaceNumber = 8;
+  const size = 10;
+  scene.onPointerObservable.add((pointerInfo) => {
+
+    // console.log("Pointer info :"+);
+    var badState = 0;
+    switch (pointerInfo.type) {
+      case BABYLON.PointerEventTypes.POINTERDOWN:
+        console.log("POINTER DOWN");
+        break;
+      case BABYLON.PointerEventTypes.POINTERUP:
+        console.log("POINTER UP");
+        break;
+      case BABYLON.PointerEventTypes.POINTERMOVE:
+        console.log("POINTER MOVE");
+        break;
+      case BABYLON.PointerEventTypes.POINTERWHEEL:
+        console.log("POINTER WHEEL");
+        break;
+      case BABYLON.PointerEventTypes.POINTERPICK:
+        console.log("POINTER PICK");
+        break;
+      case BABYLON.PointerEventTypes.POINTERTAP:
+        console.log("POINTER TAP");
+        break;
+      case BABYLON.PointerEventTypes.POINTERDOUBLETAP:
+        console.log("POINTER DOUBLE-TAP: " + badState);
+
+        var oldMaterial = pointerInfo.pickInfo.pickedMesh.material;
+
+        badState = Math.round(Math.random() * 3.3);
+        var material = new BABYLON.StandardMaterial("scene");
+        if (badState % 3 == 0 && badState != 0) {
+          material.alpha = 0.9;
+          material.diffuseColor = new BABYLON.Color3(0, 1, 0.5);
+          sphere.material = material;
+          pointerInfo.pickInfo.pickedMesh.material = material;
+        } else if (badState % 2 == 0 && badState != 0) {
+          material.alpha = 0.9;
+          material.diffuseColor = new BABYLON.Color3(1, 0, 0);
+          sphere.material = material;
+          pointerInfo.pickInfo.pickedMesh.material = material;
+        } else {
+          material.alpha = 0.3;
+          material.diffuseColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+          sphere.material = material;
+          pointerInfo.pickInfo.pickedMesh.material = material;
+        }
+        badState = badState + 1;
+        if (badState > 3) {
+          badState = 0;
+        }
+        break;
+    }
+  });
+
+  for (var k = 0; k < size; k++) {
+    for (var j = 0; j < size; j++) {
+      for (var i = 0; i < size; i++) {
+        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
+        sphere.position.y = 1 + (j * spaceNumber);
+        sphere.position.x = i * spaceNumber;
+        sphere.position.z = (k * spaceNumber);
+
+        if (i % 2 == 0) {
+          var material = new BABYLON.StandardMaterial("scene");
+          material.alpha = 0.2;
+          // material.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
+          sphere.material = material;
+        } else {
+          var material = new BABYLON.StandardMaterial("scene");
+          material.alpha = 0.3;
+          // material.diffuseColor = new BABYLON.Color3(1.0, 0.2, 0.7);
+          sphere.material = material;
+        }
+        sphereArray[i] = sphere;
+      }
+    }
+  }
+
+
   // Our built-in 'ground' shape. Params: name, options, scene
-  var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
-  
+  var ground = BABYLON.MeshBuilder.CreateGround("ground", {
+    width: 2,
+    height: 2
+  }, scene);
+
   return scene;
 }
